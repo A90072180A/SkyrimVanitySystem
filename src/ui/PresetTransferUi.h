09e499f6fc3e215746e94141982f3189b70b828e
A90072180A <38495568+A90072180A@users.noreply.github.com>
2026-09-13 23:05:47 +0200

    Add preset transfer UI interface

---
+++ src/ui/PresetTransferUi.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace sosr {
+class Menu;
+
+namespace ui::preset_transfer {
+void RequestOpen();
+void DrawDialog(Menu &a_menu);
+} // namespace ui::preset_transfer
+} // namespace sosr
